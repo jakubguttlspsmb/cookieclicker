5577635c28f9a256b8e4cf929aaf8aaf35ce75f9
Jacobguttl2006@gmail.com <Jacobguttl2006@gmail.com>
2022-10-13 08:45:30 +0200

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,42 @@
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("counter");
+const autoclicker = document.getElementById("autoclicker");
+const multiclick = document.getElementById("multiclick");
+const tur = document.getElementById("tur");
+
+let cookies = 0;
+let autoclickerPrice = 100;
+let autoclickerIncrease = 0;
+let multiclickPrice = 50;
+let multiclickIncrease = 1;
+
+cookie.onclick = () => {
+  cookies += multiclickIncrease;
+  counter.innerHTML = cookies;
+};
+
+autoclicker.onclick = () => {
+  if (cookies >= autoclickerPrice) {
+    cookies -= autoclickerPrice;
+    counter.innerHTML = cookies;
+    autoclickerPrice *= 2;
+    autoclicker.innerHTML = `Buy autoturrets: ${autoclickerPrice}`;
+    if (autoclickerIncrease == 0) {
+      setInterval(() => {
+        cookies += autoclickerIncrease;
+        counter.innerHTML = cookies;
+      }, 1000);
+    }
+    autoclickerIncrease++;
+  }
+};
+
+multiclick.onclick = () => {
+  if (cookies >= multiclickPrice) {
+    cookies -= multiclickPrice;
+    counter.innerHTML = cookies;
+    multiclickPrice *= 2;
+    multiclick.innerHTML = `Buy multishot: ${multiclickPrice}` ;
+    multiclickIncrease++;
+  }
+};
